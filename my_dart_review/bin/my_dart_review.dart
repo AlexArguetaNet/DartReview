39e7859e5bc61bc8a9a5764594afe4fa3ebd3090
Alex Argueta <alexarguetanet@gmail.com>
2025-03-25 09:50:50 -0400

    Adding project files

---
+++ my_dart_review/bin/my_dart_review.dart
@@ -0,0 +1,5 @@
+import 'package:my_dart_review/my_dart_review.dart' as my_dart_review;
+
+void main(List<String> arguments) {
+  print('Hello world: ${my_dart_review.calculate()}!');
+}
---
+++ my_dart_review/test/my_dart_review_test.dart
@@ -0,0 +1,8 @@
+import 'package:my_dart_review/my_dart_review.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
